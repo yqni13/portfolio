bc664b868010f13d76e27edd3b8062ed45b2e6ec
yqni13 <yqni13@protonmail.com>
2024-05-01 21:23:53 +0200

    add icons & colors, dummy text home comp

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  user = {
+    firstname: "Lukas",
+    lastname: "Varga",
+    alias: "yqni13"
+  }
+  
+  constructor() {}
+
 
 }
