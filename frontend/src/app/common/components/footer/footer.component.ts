be8d02c75a77bdeedbb6e4445bfe492b7f2e8296
yqni13 <yqni13@protonmail.com>
2026-03-31 02:39:22 +0200

    feat: nav + footer, styling + icons, enums

---
+++ frontend/src/app/common/components/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrl: './footer.component.scss',
+    imports: []
+})
+export class Footer {
+
+    constructor() {
+        //
+    }
+}
---
+++ frontend/src/app/common/components/navbar/navbar.component.ts
@@ -0,0 +1,84 @@
+import { AfterViewInit, Component, DOCUMENT, ElementRef, Inject, OnInit, ViewChild } from "@angular/core";
+import { ThemeOption } from "../../../shared/enums/theme-option.enum";
+import _ from "underscore";
+import { ThemeHandlerService } from "../../../shared/services/theme.service";
+import { DeviceOption } from "../../../shared/enums/device-option.enum";
+import { CommonModule } from "@angular/common";
+
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.scss',
+    imports: [
+        CommonModule
+    ]
+})
+export class Navbar implements OnInit, AfterViewInit{
+
+    @ViewChild("themeIcon") themeIcon!: ElementRef;
+
+    protected themeEnum = ThemeOption;
+    protected selectedTheme: ThemeOption;
+    protected deviceOption: DeviceOption;
+
+    private maxMobileScreenWidth: number;
+    private window: any;
+
+    constructor(
+        @Inject(DOCUMENT) private document: Document,
+        private readonly themeHandler: ThemeHandlerService,
+    ) {
+        this.selectedTheme = this.themeHandler.checkThemeSettings();
+        this.themeHandler.setThemeSettings(this.selectedTheme);
+
+        this.deviceOption = DeviceOption.DESKTOP;
+        this.maxMobileScreenWidth = 1024;
+        this.window = this.document.defaultView;
+    }
+
+    ngOnInit() {
+        if(this.window.screen !== undefined) {
+            this.setNavWidthDynamically(this.window.screen.width);
+            this.setNavWidthDynamically(this.document.body.clientWidth);        
+        }
+
+        // adapt to device screen resolution
+        const screenWidthRequestSlowedDown = _.debounce( () => {
+            this.setNavWidthDynamically(this.window.screen.width);
+        }, 250)
+        this.window.addEventListener("resize", screenWidthRequestSlowedDown, false);
+
+        // adapt to zoom level
+        const clientWidthRequestSlowedDown = _.debounce( () => {
+            this.setNavWidthDynamically(this.document.body.clientWidth);
+        }, 250)
+        this.window.addEventListener("resize", clientWidthRequestSlowedDown, false);
+    }
+
+    ngAfterViewInit() {
+        // necessary to clean and newly add classes
+        // in case of lightmode it would overwrite with both modes
+        this.applyThemeClass();
+    }
+
+    setThemeByUser() {
+        this.selectedTheme = this.themeHandler.switchTheme(this.selectedTheme);
+    }
+
+    private applyThemeClass() {
+        if(this.themeIcon) {
+            this.themeIcon.nativeElement.classList.remove(ThemeOption.DARK, ThemeOption.LIGHT);
+            this.themeIcon.nativeElement.classList.add(this.selectedTheme);
+        }
+    }
+
+    private setNavWidthDynamically(width: number) {
+        if(width > this.maxMobileScreenWidth) {
+            this.document.body.setAttribute("data-nav", 'desktopMode');
+            this.deviceOption = DeviceOption.DESKTOP;
+        } else {
+            this.document.body.setAttribute("data-nav", 'mobileMode');
+            this.deviceOption = DeviceOption.MOBILE;
+        }
+    }
+}
