9e206768deb4d4ae63606c065bd86d4aad912490
yqni13 <yqni13@protonmail.com>
2026-04-01 20:31:03 +0200

    feat: add skills, badge comp + temp backgrounds

--- frontend/src/app/modules/skills/skills.component.ts
+++ frontend/src/app/modules/skills/skills.component.ts
@@ -1,12 +1,17 @@
 import { Component } from "@angular/core";
+import { default as skillset } from "./skills.json";
+import { SkillCollection } from "../../shared/interfaces/skill.interface";
+import { BadgeComponent } from "../../common/components/badge/badge.component";
 
 @Component({
     selector: 'app-skills',
     templateUrl: './skills.component.html',
     styleUrl: './skills.component.scss',
-    imports: []
+    imports: [BadgeComponent]
 })
 export class SkillsComponent {
+
+    protected skills: SkillCollection[] = skillset;
 
     constructor() {
         //
