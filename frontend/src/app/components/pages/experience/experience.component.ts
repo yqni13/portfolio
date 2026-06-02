8b0d161a297c6f9de5019024dbb1eedf79997bd9
yqni13 <yqni13@protonmail.com>
2026-06-02 21:36:38 +0200

    feat: new toggle component + certificate prep

--- frontend/src/app/components/pages/experience/experience.component.ts
+++ frontend/src/app/components/pages/experience/experience.component.ts
@@ -1,18 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 import { default as expCollection } from "../../../data/experience.json";
 import { CommonModule } from "@angular/common";
 import { BaseComponent } from "../base.component";
 import { Experience } from "../../../utils/interfaces/experience.interface";
+import { ToggleComponent } from "../../common/toggle/toggle.component";
 
 @Component({
     selector: 'app-experience',
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        ToggleComponent
+    ],
     templateUrl: './experience.component.html',
     styleUrl: './experience.component.scss',
 })
 export class ExperienceComponent extends BaseComponent {
 
     protected experiences: Experience[] = expCollection;
+    protected readonly showCertifications = signal(false);
 
     constructor() {
         super();
@@ -20,5 +25,9 @@ export class ExperienceComponent extends BaseComponent {
             title: 'Experience',
             subTitle: 'My professional journey.'
         };
+    }
+
+    mapExperienceList(status: boolean) {
+        this.showCertifications.set(status);
     }
 }
